57706fdcb0da611dc908120cbfdc797d9090fcb0
jkooyoung <rndud2008@naver.com>
2024-07-31 18:27:35 +0900

    repository / service 생성.

---
+++ SpringWork/src/main/java/com/lec/spring/service/AttachmentService.java
@@ -0,0 +1,18 @@
+package com.lec.spring.service;
+
+import com.lec.spring.repository.AttachmentRepository;
+import com.lec.spring.repository.PostRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AttachmentService {
+
+    private  final PostRepository postRepository;
+    private  final AttachmentRepository attachmentRepository;
+
+    public AttachmentService(PostRepository postRepository, AttachmentRepository attachmentRepository) {
+        this.postRepository = postRepository;
+        this.attachmentRepository = attachmentRepository;
+    }
+
+}
