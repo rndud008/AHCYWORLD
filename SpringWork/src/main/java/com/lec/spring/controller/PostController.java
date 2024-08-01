912ad9f991e0c58aefda39064a98d8a380f09c08
jkooyoung <rndud2008@naver.com>
2024-08-01 12:32:31 +0900

    controller 작성중

--- SpringWork/src/main/java/com/lec/spring/controller/PostController.java
+++ SpringWork/src/main/java/com/lec/spring/controller/PostController.java
@@ -1,9 +1,48 @@
 package com.lec.spring.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.lec.spring.domain.Post;
+import com.lec.spring.service.PostService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
 public class PostController {
+
+    private final PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    // 작성
+    @PostMapping("/{hompyId}/write")
+    public ResponseEntity<?> write(@PathVariable Long hompyId
+            , @RequestBody Post post
+            , HttpServletRequest request){
+
+        String authorization  = request.getHeader("Authorization");
+        if(authorization == null || !authorization.startsWith("Bearer")){
+            return null;
+        }
+
+        String token = authorization.split(" ")[1];
+
+
+
+
+
+
+
+        return null;
+    }
+
+    // 수정
+
+    // 삭제
+
+    // 조회
+
+    // list?
 }
