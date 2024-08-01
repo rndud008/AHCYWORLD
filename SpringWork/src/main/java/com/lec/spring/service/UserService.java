e3345abf88156e796b21caa312c78a55f0ac9de8
Han-DDong <jungwoo03173288@gmail.com>
2024-08-01 12:01:19 +0900

    # 미니홈피 # 댓글

--- SpringWork/src/main/java/com/lec/spring/service/UserService.java
+++ SpringWork/src/main/java/com/lec/spring/service/UserService.java
@@ -1,7 +1,24 @@
 package com.lec.spring.service;
 
+import com.lec.spring.domain.User;
+import com.lec.spring.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    // 특정 ID로 User 조회
+    public Optional<User> findByUserId(Long id) {
+        return userRepository.findById(id);
+    }
 }
