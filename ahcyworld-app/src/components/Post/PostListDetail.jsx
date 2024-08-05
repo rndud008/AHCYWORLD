1bcd23eeb3ea8ca8427e8928c9feff249e8f880f
jkooyoung <rndud2008@naver.com>
2024-08-05 20:27:39 +0900

    게시글 기능 스크랩 제외 / 완료.

--- ahcyworld-app/src/components/Post/PostListDetail.jsx
+++ ahcyworld-app/src/components/Post/PostListDetail.jsx
@@ -1,7 +1,11 @@
 import React, { useEffect } from "react";
-import { Button, Col, Container, Row } from "react-bootstrap";
+import { Button, Col, Container, Row, Table } from "react-bootstrap";
+import { Link, useParams } from "react-router-dom";
+import PageNation from "./PageNation";
+import PostItem from "./PostItem";
+import PostListDetailItem from "./PostListDetailItem";
 
-const PostListDetail = ({ name, pageAndPostList ,folder}) => {
+const PostListDetail = ({ pageAndPostList, folder , setPage, folderList, moveFolderId, setMoveFolderId}) => {
   console.log("pageAndPostList first", pageAndPostList);
   console.log(
     "pageAndPostList second:",
@@ -10,16 +14,37 @@ const PostListDetail = ({ name, pageAndPostList ,folder}) => {
       : "게시물이 존재하지 않습니다."
   );
 
-  console.log('pageAndPostList third' , folder)
+  const { postName, hompyId } = useParams();
+
+  console.log("pageAndPostList third", folder);
   return (
     <>
-      
       <Container>
         <div className="postListHeader">
-        {folder[0] && <div>{folder[0].name}</div>}
-        {folder[0] && <div><Button>글쓰기</Button></div>}
+          {folder[0] && <div>{folder[0].name}</div>}
+          {folder[0] && (
+            <div>
+              <Button variant="none">
+                {" "}
+                <Link to={`/post/${hompyId}/${postName}/${folder[0].id}/write`}>
+                {postName} 올리기
+                </Link>
+              </Button>
+            </div>
+          )}
         </div>
-        <div></div>
+        <div>
+            {pageAndPostList?.posts !=null ? (
+              pageAndPostList?.posts?.map((item) => {
+                return <PostListDetailItem item={item} folderList={folderList} moveFolderId={moveFolderId} setMoveFolderId={setMoveFolderId}/>;
+              })
+            ) : (
+               <h4>게시물이 존재하지 않습니다.</h4>
+            )}
+        </div>
+        <div>
+          <PageNation pageAndPostList={pageAndPostList} setPage={setPage} />
+        </div>
       </Container>
     </>
   );
