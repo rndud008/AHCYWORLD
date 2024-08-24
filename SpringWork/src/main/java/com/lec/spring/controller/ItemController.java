f90c607ad5a198328d486554ab4e2874186c8945
sejin97 <kaekwam@gmail.com>
2024-08-24 15:21:48 +0900

    폴더 위치 수정

--- SpringWork/src/main/java/com/lec/spring/controller/ItemController.java
+++ SpringWork/src/main/java/com/lec/spring/controller/ItemController.java
@@ -84,7 +84,7 @@ public class ItemController {
 
 
 //        Path Dir = Paths.get("../ahcyworld-app/public", "image").toAbsolutePath();        // 로컬호스트의 파일 경로
-        Path Dir = Paths.get("../../build/", "image").toAbsolutePath(); //aws서버의 파일 경로
+        Path Dir = Paths.get("/build/", "image").toAbsolutePath(); //aws서버의 파일 경로
         System.out.println("2.경로"+ Dir);
         System.out.println("3. 이미 있는 폴더? : "+Files.isDirectory(Dir));
         if (Files.isDirectory(Dir)) {
@@ -110,7 +110,7 @@ public class ItemController {
                 }
             }
 //            String uploadDir = Paths.get("../ahcyworld-app/public", "image").toAbsolutePath().toString();   //로컬호스트 파일 경로
-            String uploadDir = Paths.get("../../build/", "image").toAbsolutePath().toString();   //aws 서버 파일 경로
+            String uploadDir = Paths.get("build/", "image").toAbsolutePath().toString();   //aws 서버 파일 경로
 
             System.out.println("4.업로드될 폴더 경로"+uploadDir);
             // 실제 파일 저장
